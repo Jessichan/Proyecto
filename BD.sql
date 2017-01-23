b1b4fbe283aff72dd862a26815e531fd48cb77e1
jessica ferrera <jessicaferrera16@gmail.com>
2017-01-23 20:48:12 +0100

    BD terminada

--- BD.sql
+++ BD.sql
@@ -7,7 +7,7 @@ GRANT ALL PRIVILEGES ON animalshop* TO 'mascota'@'localhost';
 FLUSH PRIVILEGES;
 
 
--- Tables's creation
+-- Crear tabla
 create table cliente
 (
 	idcliente		int auto_increment not null,
@@ -35,8 +35,10 @@ create table animal
     idanimal        int auto_increment not null,
     especie      	varchar(20),
     nombre	      	varchar(25),
-    raza  			varchar(25),
-    precio      	decimal(7,2),
+    raza  			varchar(50),
+    edad			varchar(10),
+    descripcion		varchar(500),
+    precio      	decimal(3,2),
     primary key(idanimal)
  );
 
@@ -44,7 +46,7 @@ create table animal
 create table tiene
 (
 	idanimal		int,
-	idalquiler		int
+	idalquiler		int,
 	cantidad		int,
 	primary key(idanimal, idalquiler),
 	foreign key(idanimal) references animal(idanimal),
@@ -54,9 +56,10 @@ create table tiene
 create table accesorio
 (
 	idaccesorio		int auto_increment not null,
-	nombre			varchar(25),
+	nombre			varchar(50),
+	descripcion		varchar(50),
 	cantidad		int,
-	precio			decimal(7,2),
+	precio			decimal(3,2),
 	primary key(idaccesorio)
 	);
 
@@ -70,3 +73,48 @@ create table compra
 	foreign key(idcliente) references cliente(idcliente),
 	foreign key(idaccesorio) references accesorio(idaccesorio) on delete cascade
 );
+
+-- insertar datos
+insert into cliente values(null, 'Maria', 'Gonzalez', '657890634','mariag@gmail.com','admin', 'Admin', '1234');
+insert into cliente values(null, 'Carlos', 'Martin', '654321672', 'carlosmar@gmail', 'luism', 'User', '168923');
+insert into cliente values(null, 'Amanda', 'Marin', '647896541', 'amanda23@gmail.com', 'amanda', 'User', '123456');
+insert into cliente values(null, 'Jose', 'Laguillo', '654789064', 'josela@gmail.com', 'josel', 'User', 'asdfgh');
+insert into cliente values(null, 'Jonni', 'Melavo', '654378906', 'joni@gmail.com', 'jonim', 'User', 'jonim');
+insert into cliente values(null, 'Holi', 'Moli', '675432349', 'holimoli@hotmail.com', 'holim', 'User', 'holim');
+
+insert into alquiler values(null, 3, now());
+insert into alquiler values(null, 5, now());
+insert into alquiler values(null, 2, now());
+insert into alquiler values(null, 4, now());
+insert into alquiler values(null, 6, now());
+
+insert into animal values(null, 'perro', 'Peludo', 'Golden Retriever', '3 meses', 'Perro cariñoso y jugueton', 18.00);
+insert into animal values(null, 'gato', 'doraemon', 'Bengala', '4 meses', 'Gato jugueton y gracioso', 20.00);
+insert into animal values(null, 'pajaro', 'Manuel', 'Canario', '6 meses', 'ya sabe volar' 15.00);
+insert into animal values(null, 'loro', 'Tina', 'Psittacus erithacus', '2 meses', 'Tararea el himno de España', 23.00);
+insert into animal values(null, 'perro', 'Giva', 'Dalmata', '1 año'  'muy mansito', 17.00);
+insert into animal values(null, 'conejo', 'Sabana', 'Belier Gold', 'año y medio', 'Se deja mimar que da gusto', 14.00);
+insert into animal values(null, 'hamster', 'Currito', 'Hanster Comun', '1 año' , 'Se esta preparando para la carrera nocturna', 12.50);
+insert into animal values(null, 'gato', 'Mini', 'Bombai', '1 año', 'Gato cariñoso y goloso', 18.00);
+
+insert into tiene values(3, 4, 1);
+insert into tiene values(2, 2, 1);
+insert into tiene values(8, 2, 1);
+insert into tiene values(5, 3, 1);
+insert into tiene values(4, 5, 1);
+insert into tiene values(6, 1, 1);
+
+insert into accesorio values(null, 'Correa','Correa para perros' , 10, 3.00);
+insert into accesorio values(null, 'sonajero', 'Juguete para gatos o perros', 10, 2.50);
+insert into accesorio values(null, 'Hueso comestible', 'hueso sabroso para perros', 20, 4.00);
+insert into accesorio values(null, 'Brekkies Excel Junior', 'Comida para perros pequeños', 10, 7.00);
+insert into accesorio values(null, 'Brekiies Excel Senior', 'Comida para perros de mas de 7 años', 10, 7.50);
+insert into accesorio values(null, 'Brekkies Excel Cat', 'Comida sabrosa para gatos', 10, 8.00);
+insert into accesorio values(null, 'vitakraft menu', 'Comida para pequeños roedores', 10, 8.00);
+insert into accesorio values(null, 'Pio pa', 'Mezcla para canarios', 10, 1.77);
+
+insert into compra values(3, 7, 1, 8.00);
+insert into compra values(2, 4, 1, 7.00);
+insert into compra values(4, 8, 1, 1.77);
+insert into compra values(5, 2, 1, 2.50);
+insert into compra values(6, 2, 1, 2.50);
