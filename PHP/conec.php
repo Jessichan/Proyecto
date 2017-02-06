39c65f39ace194d33da344141961d5777201e62a
jessica ferrera <jessicaferrera16@gmail.com>
2017-02-06 10:42:20 +0100

    utf8

--- PHP/conec.php
+++ PHP/conec.php
@@ -7,7 +7,7 @@
 <?php
 //CREATING THE CONNECTION
           $connection = new mysqli("localhost", "mascota", "minino", "animalshop");
-
+          $connection->set_charset("utf8");
           //TESTING IF THE CONNECTION WAS RIGHT
           if ($connection->connect_errno) {
               printf("Connection failed: %s\n", $connection->connect_error);
